e9dc0e63cfcb43011e7d057235af980e1a8fd529
KIISA <nyakatyan@bk.ru>
2020-04-04 00:39:01 +0300

    update 7.1

--- LW7/script1.js
+++ LW7/script1.js
@@ -19,17 +19,18 @@ function calculateNewPrimeNumber() {
 
 function isPrimeNumber(numbers) {
 	if (Number.isInteger(numbers)) {
-		let prime = true;
-		numbers = Math.abs(numbers);
+		let prime = numbers >= 0 ? true : false;
+		if (prime && (numbers < 2)) {
+			console.log('Вы ввели ни простоe, ни составное число.... та.... такие существуют');
+			return;
+		}
 		for (let index = 0; index < primeNumbers.length; index++) {
+			if (!prime) break;
 			let sqrtNum = Math.sqrt(numbers);
 			let currentElement = primeNumbers[index];
 			if (primeNumbers[primeNumbers.length-1] < sqrtNum) calculateNewPrimeNumber();
 			if (sqrtNum < currentElement) break;
-			if (numbers % currentElement === 0) {
-				prime = false;
-				break
-			}
+			if (numbers % currentElement === 0) prime = false;
 		}
 		let response = prime ? "is prime" : "is not prime";
 		console.log(`Результут: ${numbers} ${response} number`);
@@ -37,5 +38,7 @@ function isPrimeNumber(numbers) {
 		for (number of numbers) {
 			isPrimeNumber(number);
 		}
+	} else {
+		console.log(`Данные: ${numbers} не являются числом или массивом`);
 	}
 }
